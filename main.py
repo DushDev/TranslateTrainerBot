ccd87b192de1f9f59280b3e63cd0ce5454b85ca0
DushDev <imthedush@gmail.com>
2023-09-09 10:48:55 +0300

    base

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# нужено заимпортить телебота и дотенв
+import telebot
+bot = telebot.TeleBot("6492917074:AAH3f6MPYHEM-z7yN4PoS-kbTGE77YgSWXs")
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты напили /start')
+
+bot.polling(none_stop=True)
